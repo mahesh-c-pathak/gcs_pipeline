4a44c0eac43e145eb81d6d02bc5aa03035893ee3
mahesh-c-pathak <93431683+mahesh-c-pathak@users.noreply.github.com>
2024-03-04 11:27:42 +0530

    Delete udf.js

--- udf.js
+++
@@ -1,9 +0,0 @@
-function transform(line) {
-    var values = line.split(',');
-    var obj = new Object();
-    obj.name = values[0];
-    obj.country = values[1];
-    obj.created_at = values[2];
-    var jsonString = JSON.stringify(obj);
-    return jsonString;
-   }
